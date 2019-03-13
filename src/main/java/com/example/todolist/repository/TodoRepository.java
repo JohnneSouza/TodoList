ddbf693bb267aaa7b6e687790ae2e7e0774ee3b0
Johnnes Souza <jhonnesf5@gmail.com>
2019-03-13 09:10:42 -0300

    Project Exported to Zip

--- src/main/java/com/example/todolist/repository/TodoRepository.java
+++ src/main/java/com/example/todolist/repository/TodoRepository.java
@@ -1,12 +1,10 @@
 package com.example.todolist.repository;
 
 import com.example.todolist.model.Todo;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -14,7 +12,7 @@ public interface TodoRepository extends PagingAndSortingRepository<Todo, Long> {
 
     @Query(value = "select t from Todo t where t.title = ?1",
     countQuery = "select count(t) from Todo t where t.title = ?1")
-    List<Todo> findByTitle (String title, PageRequest pageRequest);
+    Page<Todo> findByTitle (String title, PageRequest pageRequest);
 
     @Query("select t from Todo t where t.title = ?1 and t.id = ?2")
     List<Todo> findByTitleAndId (String title, Long id);
